398618771b93ded8722fa211fee732e3243022ae
unknown <zhaopudark@outlook.com>
2023-07-27 09:41:30 +0800

    Modify workflows

--- 0.0.1/tests/PathTools.Tests.ps1
+++ 0.0.1/tests/PathTools.Tests.ps1
@@ -1,5 +1,5 @@
 BeforeAll {
-    Import-Module "${PSScriptRoot}\..\PSComputerManagementZp.psm1" -Force
+    Import-Module PSComputerManagementZp -Force
     $guid = [guid]::NewGuid()
     $test_path = "${Home}\$guid"
     New-Item -Path $test_path -ItemType Directory -Force
--- 0.0.1/tests/ProxyTools.Tests.ps1
+++ 0.0.1/tests/ProxyTools.Tests.ps1
@@ -4,25 +4,25 @@ BeforeAll {
     $process_env_all_proxy_backup = [Environment]::GetEnvironmentVariable('all_proxy')
     $process_env_ftp_proxy_backup = [Environment]::GetEnvironmentVariable('ftp_proxy')
     $process_env_socks_proxy_backup = [Environment]::GetEnvironmentVariable('socks_proxy')
-    Import-Module "${PSScriptRoot}\..\PSComputerManagementZp.psm1" -Force
+    Import-Module PSComputerManagementZp -Force
 }
 
 Describe 'Test ProxyTools' {
-    It 'Test Set-SystemProxyIPV4ForCurrentUser' {
-        Set-SystemProxyIPV4ForCurrentUser -ServerIP 127.1.2.3 -PortNumber 4567.
-        [Environment]::GetEnvironmentVariable('http_proxy') | Should -Be "http://127.1.2.3:4567"
-        [Environment]::GetEnvironmentVariable('https_proxy') | Should -Be "http://127.1.2.3:4567"
-        [Environment]::GetEnvironmentVariable('all_proxy') | Should -Be "http://127.1.2.3:4567"
-        [Environment]::GetEnvironmentVariable('ftp_proxy') | Should -Be "http://127.1.2.3:4567"
-        [Environment]::GetEnvironmentVariable('socks_proxy') | Should -Be "socks5://127.1.2.3:4567" 
+    It 'Test Set-EnvProxyIPV4ForShellProcess' {
+        Set-EnvProxyIPV4ForShellProcess -ServerIP 127.1.2.3 -PortNumber 4567.
+        [Environment]::GetEnvironmentVariable('http_proxy') | Should -Be 'http://127.1.2.3:4567'
+        [Environment]::GetEnvironmentVariable('https_proxy') | Should -Be 'http://127.1.2.3:4567'
+        [Environment]::GetEnvironmentVariable('all_proxy') | Should -Be 'http://127.1.2.3:4567'
+        [Environment]::GetEnvironmentVariable('ftp_proxy') | Should -Be 'http://127.1.2.3:4567'
+        [Environment]::GetEnvironmentVariable('socks_proxy') | Should -Be 'socks5://127.1.2.3:4567' 
     }
     It 'Test Remove-EnvProxyIPV4ForShellProcess' {
         Remove-EnvProxyIPV4ForShellProcess
-        [Environment]::GetEnvironmentVariable('http_proxy') | Should -Be ''
-        [Environment]::GetEnvironmentVariable('https_proxy') | Should -Be ''
-        [Environment]::GetEnvironmentVariable('all_proxy') | Should -Be ''
-        [Environment]::GetEnvironmentVariable('ftp_proxy') | Should -Be ''
-        [Environment]::GetEnvironmentVariable('socks_proxy') | Should -Be ''
+        [Environment]::GetEnvironmentVariable('http_proxy') | Should -Be $null
+        [Environment]::GetEnvironmentVariable('https_proxy') | Should -Be $null
+        [Environment]::GetEnvironmentVariable('all_proxy') | Should -Be $null
+        [Environment]::GetEnvironmentVariable('ftp_proxy') | Should -Be $null
+        [Environment]::GetEnvironmentVariable('socks_proxy') | Should -Be $null
     }
 
 }
