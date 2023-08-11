79055d3ccade98d641c6f89e650f7ce735007951
unknown <zhaopudark@outlook.com>
2023-08-11 12:47:47 +0800

    Check with PSScriptAnalyzer and modify

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 $version = Get-ChildItem -Directory $PSScriptRoot | Where-Object { $_.Name -match '^\d' } | Select-Object -ExpandProperty Name
 
-Import-Module "./$version/helpers/PlatformTools.psm1" -Force -Scope Local
+Import-Module "${PSScriptRoot}/$version/helpers/PlatformTools.psm1" -Force -Scope Local
 
 
 $module_install_path = Get-InstallPath
@@ -18,4 +18,3 @@ $source_paths = Get-ChildItem $PSScriptRoot -Exclude ".*"
 
 Copy-Item -Path $source_paths -Destination $module_install_path -Recurse -Force
 
-Remove-Module "PlatformTools"
