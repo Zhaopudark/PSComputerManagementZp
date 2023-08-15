2c1367dcb3b1324b01db8c63052078e912b78b56
unknown <zhaopudark@outlook.com>
2023-08-15 12:23:30 +0800

    Check with PSScriptAnalyzer and modify

--- 0.0.1/tests/LinkTools.Tests.ps1
+++ 0.0.1/tests/LinkTools.Tests.ps1
@@ -6,14 +6,14 @@ BeforeAll {
     New-Item -Path $test_path -ItemType Directory -Force
     New-Item -Path "$test_path\file_for_symbolic_link.txt" -ItemType File
     New-Item -Path "$test_path\file_for_hard_link.txt" -ItemType File
-    New-Item -Path "$test_path\test_junction\dir1" -ItemType Directory
-    New-Item -Path "$test_path\test_junction\dir1\file1.txt" -ItemType File
-    New-Item -Path "$test_path\test_junction\dir1\dir1" -ItemType Directory
-    New-Item -Path "$test_path\test_junction\dir1\dir1\file11.txt" -ItemType File
-    New-Item -Path "$test_path\test_junction\dir2" -ItemType Directory
-    New-Item -Path "$test_path\test_junction\dir2\file2.txt" -ItemType File
-    New-Item -Path "$test_path\test_junction\dir2\dir2" -ItemType Directory
-    New-Item -Path "$test_path\test_junction\dir2\dir2\file22.txt" -ItemType File
+    New-Item -Path "$test_path\test_junction\dir3" -ItemType Directory
+    New-Item -Path "$test_path\test_junction\dir3\file3.txt" -ItemType File
+    New-Item -Path "$test_path\test_junction\dir3\dir3" -ItemType Directory
+    New-Item -Path "$test_path\test_junction\dir3\dir3\file33.txt" -ItemType File
+    New-Item -Path "$test_path\test_junction\dir4" -ItemType Directory
+    New-Item -Path "$test_path\test_junction\dir4\file4.txt" -ItemType File
+    New-Item -Path "$test_path\test_junction\dir4\dir4" -ItemType Directory
+    New-Item -Path "$test_path\test_junction\dir4\dir4\file44.txt" -ItemType File
     New-Item -Path "$test_path\test_symbolick_dir\dir1" -ItemType Directory
     New-Item -Path "$test_path\test_symbolick_dir\dir1\file1.txt" -ItemType File
     New-Item -Path "$test_path\test_symbolick_dir\dir1\dir1" -ItemType Directory
@@ -49,11 +49,37 @@ Describe 'Link EnvTools' {
             "$backup2\dir2" | Should -Exist
             "$backup2\dir2\file22.txt" | Should -Exist
         }
+        It 'Test Set-DirJunctionWithSync' {
+            Set-DirJunctionWithSync -Path "$test_path\test_junction\dir3"  -Target "$test_path\test_junction\dir4" -Backuppath "$test_path\backup"
+            $item = Get-ItemProperty "$test_path\test_junction\dir3"
+            $item.LinkType | Should -Be 'Junction'
+            $item.LinkTarget | Should -Be "$test_path\test_junction\dir4"
+            "$test_path\test_junction\dir3" | Should -Exist
+            "$test_path\test_junction\dir4\file3.txt" | Should -Exist
+            "$test_path\test_junction\dir4\dir3" | Should -Exist
+            "$test_path\test_junction\dir4\dir3\file33.txt" | Should -Exist
+            "$test_path\test_junction\dir4" | Should -Exist
+            "$test_path\test_junction\dir4\file4.txt" | Should -Exist
+            "$test_path\test_junction\dir4\dir4" | Should -Exist
+            "$test_path\test_junction\dir4\dir4\file44.txt" | Should -Exist
+            $backup3 = (Get-ChildItem "$test_path\backup"  -Filter "*dir3")[0]
+            $backup4 = (Get-ChildItem "$test_path\backup"  -Filter "*dir4")[0]
+            "$backup3\file3.txt" | Should -Exist
+            "$backup3\dir3" | Should -Exist
+            "$backup3\dir3\file33.txt" | Should -Exist
+            "$backup4\file4.txt" | Should -Exist
+            "$backup4\dir4" | Should -Exist
+            "$backup4\dir4\file44.txt" | Should -Exist
+        }
     }
     Context 'On non-Windows' -Skip:(Test-Platform 'Windows'){
         It 'Test Set-DirSymbolicLinkWithSync' {
             {Set-DirSymbolicLinkWithSync -Path "$test_path\test_symbolick_dir\dir1"  -Target "$test_path\test_symbolick_dir\dir2" -Backuppath "$test_path\backup" }| Should -Throw
         }
+        It 'Test Set-DirJunctionWithSync' {
+            {Set-DirJunctionWithSync -Path "$test_path\test_symbolick_dir\dir3"  -Target "$test_path\test_symbolick_dir\dir4" -Backuppath "$test_path\backup" }| Should -Throw
+        }
+
     }
 
 }
