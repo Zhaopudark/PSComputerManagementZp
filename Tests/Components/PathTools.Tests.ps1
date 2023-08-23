dcc7a321b297a57254a321b73d69e629257ba283
unknown <zhaopudark@outlook.com>
2023-08-24 05:31:33 +0800

    Check with PSScriptAnalyzer and modify

--- Tests/Components/PathTools.Tests.ps1
+++ Tests/Components/PathTools.Tests.ps1
@@ -1,34 +1,24 @@
-BeforeAll {
-    foreach ($item in (Get-ChildItem "${PSScriptRoot}\..\..\Module" -Filter *.ps1)){
-        Import-Module $item.FullName -Force -Scope Local
-    }
-    . "${PSScriptRoot}\..\..\Module\Config.ps1"
+$sb = {
+    param(
+        $IsxWIndows,
+        $IsxLinux,
+        $IsxWsl2)
 
-    $guid = [guid]::NewGuid()
-    $test_path = "${Home}\$guid"
-    New-Item -Path $test_path -ItemType Directory -Force
-
-    $test_dir = "${test_path}\test_dir"
-    $test_file = "${test_path}\test.txt"
-    New-Item -Path $test_dir -ItemType Directory -Force
-    New-Item -Path $test_file -ItemType File -Force
-}
-
-Describe 'Test PathTools' {
+    Describe 'Test PathTools' {
     Context 'Test Format-LiteralPath' {
-        It 'Test on Windows dir' -Skip:(!(Test-Platform 'Windows')){
+        It 'Test on Windows dir' -Skip:(!$IsxWIndows){
             $path = [FormattedPath]::new("${test_path}\tEsT_diR")
             $path | Should -BeExactly "$([FormattedPath]::new($test_path))test_dir\"
         }
-        It 'Test on Linux dir' -Skip:(!(Test-Platform 'Linux')){
+        It 'Test on Linux dir' -Skip:(!$IsxLinux){
             $path = [FormattedPath]::new("${test_path}\test_dir")
             $path | Should -BeExactly "$([FormattedPath]::new($test_path))test_dir/"
         }
-        It 'Test on Wsl2 dir' -Skip:(!(Test-Platform 'Wsl2')){
+        It 'Test on Wsl2 dir' -Skip:(!$IsxWsl2){
             $path = [FormattedPath]::new("${test_path}\test_dir")
             $path | Should -BeExactly "$([FormattedPath]::new($test_path))test_dir/"
         }
-        It 'Test on windows drive' -Skip:(!(Test-Platform 'Windows')){
+        It 'Test on windows drive' -Skip:(!$IsxWIndows){
 
             $maybe_c = (Get-ItemProperty ${Home}).PSDrive.Name
             $maybe_c_lower = (Get-ItemProperty ${Home}).PSDrive.Name.ToLower()
@@ -51,7 +41,7 @@ Describe 'Test PathTools' {
             $path = [FormattedPath]::new("$maybe_c_lower`:")
             $path | Should -BeExactly "$maybe_c`:\"
         }
-        It 'Test on Linux drive' -Skip:(!(Test-Platform 'Linux')){
+        It 'Test on Linux drive' -Skip:(!$IsxLinux){
             $maybe_root = (Get-ItemProperty ${Home}).PSDrive.Name # / on Linux and Wsl2, not '/root'
             $path = [FormattedPath]::new("$maybe_root")
             $path | Should -BeExactly "$maybe_root"
@@ -68,7 +58,7 @@ Describe 'Test PathTools' {
             $path = [FormattedPath]::new("$maybe_root/\/\/\/\/\")
             $path | Should -BeExactly "$maybe_root"
         }
-        It 'Test on Wsl2 drive' -Skip:(!(Test-Platform 'Wsl2')){
+        It 'Test on Wsl2 drive' -Skip:(!$IsxWsl2){
             $maybe_root = (Get-ItemProperty ${Home}).PSDrive.Name # / on Linux and Wsl2, not '/root'
             $path = [FormattedPath]::new("$maybe_root")
             $path | Should -BeExactly "$maybe_root"
@@ -85,12 +75,34 @@ Describe 'Test PathTools' {
             $path = [FormattedPath]::new("$maybe_root/\/\/\/\/\")
             $path | Should -BeExactly "$maybe_root"
         }
+    }  
     }
-    
-
 }
 
-AfterAll {
-    Remove-Item -Path $test_path -Force -Recurse
-    Remove-Module PSComputerManagementZp -Force
-}
+
+. "${PSScriptRoot}\..\..\Module\Config.ps1"
+
+$guid = [guid]::NewGuid()
+$test_path = "${Home}\$guid"
+New-Item -Path $test_path -ItemType Directory -Force
+
+$test_dir = "${test_path}\test_dir"
+$test_file = "${test_path}\test.txt"
+New-Item -Path $test_dir -ItemType Directory -Force
+New-Item -Path $test_file -ItemType File -Force
+
+$IsxWIndows = Test-Platform 'Windows'
+$IsxLinux =  Test-Platform 'Linux'
+$IsxWsl2 =  Test-Platform 'Wsl2'
+
+$container = New-PesterContainer -ScriptBlock $sb -Data @{ 
+        IsxWIndows = $IsxWIndows
+        IsxLinux = $IsxLinux
+        IsxWsl2 = $IsxWsl2}
+$configuration = New-PesterConfiguration
+$configuration.Run.Container = $container
+$configuration.Output.Verbosity = 'Detailed'
+
+Invoke-Pester -Configuration $configuration
+
+Remove-Item -Path $test_path -Force -Recurse
