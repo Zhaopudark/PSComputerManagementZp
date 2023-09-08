0b896e704f0916d9d3f440b6d18538d28cd216de
unknown <zhaopudark@outlook.com>
2023-09-08 14:55:52 +0800

    Prepare for release v0.0.3

--- Module/Private/Tools.Copy.ps1
+++ Module/Private/Tools.Copy.ps1
@@ -26,23 +26,24 @@ function Copy-FileWithBackup{
     if($Path.IsSymbolicLink -or $Path.IsHardLink){
         throw "The $Path should not be a symbolic link or a hard link."
     }
-    $_source = $Path
-    $_destination = $Destination
-    if ($PSCmdlet.ShouldProcess("Then copy $_source to $_destination. Backup $_destination if it exists already.",'','')){
+    $source = $Path
+    $destination = $Destination
+    if ($PSCmdlet.ShouldProcess("Then copy $source to $destination. Backup $destination if it exists already.",'','')){
         if(Test-Path $_destination){
-            $_destination = Get-FormattedFileSystemPath -Path $_destination
-            if(!$_destination.IsFile){
-                throw "The $_destination should be a file."
+            # see https://stackoverflow.com/a/77062276/17357963
+            [psobject] $destination = Get-FormattedFileSystemPath -Path $destination
+            if(!$destination.IsFile){
+                throw "The $destination should be a file."
             }
-            if($_destination.IsSymbolicLink -or $_destination.IsHardLink){
-                throw "The $_destination should not be a symbolic link or a hard link."
+            if($destination.IsSymbolicLink -or $destination.IsHardLink){
+                throw "The $destination should not be a symbolic link or a hard link."
             }
-            $_destination_backup = "$BackupDir/$Indicator-$($_destination.ToShortName())"
-            Write-Logs "Copy-Item -Path $_destination -Destination $_destination_backup -Force"
-            Copy-Item -Path $_destination -Destination $_destination_backup -Force
+            $destination_backup = "$BackupDir/$Indicator-$($destination.ToShortName())"
+            Write-Logs "Copy-Item -Path $destination -Destination $destination_backup -Force"
+            Copy-Item -Path $destination -Destination $destination_backup -Force
         }else{
-            Write-Logs "Copy-Item -Path $_source -Destination $_destination -Force"
-            Copy-Item -Path $_source -Destination $_destination -Force
+            Write-Logs "Copy-Item -Path $source -Destination $destination -Force"
+            Copy-Item -Path $source -Destination $destination -Force
         }
     }
 }
@@ -79,26 +80,26 @@ function Copy-DirWithBackup{
     if($Path.IsSymbolicLink -or $Path.IsJunction){
         throw "The $Path should not be a symbolic link or a junction point."
     }
-    $_source = $Path
-    $_destination = $Destination
-    if ($PSCmdlet.ShouldProcess("Then copy $_source to $_destination. Backup $_source and $_destination if the latter exists already.",'','')){
-        if(Test-Path $_destination){
-            $_source_backup = "$BackupDir/$Indicator-$($_source.ToShortName())"
-
-            $_destination = Get-FormattedFileSystemPath -Path $_destination
-            if(!$_destination.IsDir){
-                throw "The $_destination should be a directory."
+    $source = $Path
+    $destination = $Destination
+    if ($PSCmdlet.ShouldProcess("Then copy $source to $destination. Backup $source and $destination if the latter exists already.",'','')){
+        if(Test-Path $destination){
+            $source_backup = "$BackupDir/$Indicator-$($source.ToShortName())"
+            # see https://stackoverflow.com/a/77062276/17357963
+            [psobject] $destination = Get-FormattedFileSystemPath -Path $destination
+            if(!$destination.IsDir){
+                throw "The $destination should be a directory."
             }
-            if($_destination.IsSymbolicLink -or $_destination.IsJunction){
-               throw "The $_destination should not be a symbolic link or a junction point."
+            if($destination.IsSymbolicLink -or $destination.IsJunction){
+               throw "The $destination should not be a symbolic link or a junction point."
             }
-            $_destination_backup = "$BackupDir/$Indicator-$($_destination.ToShortName())"
+            $destination_backup = "$BackupDir/$Indicator-$($destination.ToShortName())"
             try {
                 Assert-AdminRobocopyAvailable
                 $log_file = Get-LogFileName "Robocopy Copy-DirWithBackup"
-                Robocopy $_source $_source_backup /e /copyall /dcopy:DATE /log+:"$log_file"
-                Robocopy $_destination $_destination_backup /e /copyall /dcopy:DATE /log+:"$log_file"
-                Robocopy $_source $_destination /e /copyall /dcopy:DATE /log+:"$log_file"
+                Robocopy $source $source_backup /e /copyall /dcopy:DATE /log+:"$log_file"
+                Robocopy $destination $destination_backup /e /copyall /dcopy:DATE /log+:"$log_file"
+                Robocopy $source $destination /e /copyall /dcopy:DATE /log+:"$log_file"
                 # https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/robocopy
                 # /e	            Copies subdirectories. This option automatically includes empty directories.
                 # /copyall	        Copies all file information (equivalent to /copy:DATSOU).
@@ -108,20 +109,20 @@ function Copy-DirWithBackup{
             catch {
                 Write-Logs "[Try Robocopy Failed] Use Copy-Item instead. But Copy-Item will loss the metadata of directories in the source."
                 
-                New-Item -Path $_source_backup -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-                Write-Logs " Copy-Item -Path `"$_source\*`" -Destination $_source_backup -Recurse -Force"
-                Copy-Item -Path "$_source\*" -Destination $_source_backup -Recurse -Force
+                New-Item -Path $source_backup -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+                Write-Logs " Copy-Item -Path `"$source\*`" -Destination $source_backup -Recurse -Force"
+                Copy-Item -Path "$source\*" -Destination $source_backup -Recurse -Force
 
-                New-Item -Path $_destination_backup -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
-                Write-Logs "Copy-Item -Path `"$_destination\*`" -Destination $_destination_backup -Recurse -Force"
-                Copy-Item -Path "$_destination\*" -Destination $_destination_backup -Recurse -Force
+                New-Item -Path $destination_backup -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+                Write-Logs "Copy-Item -Path `"$destination\*`" -Destination $destination_backup -Recurse -Force"
+                Copy-Item -Path "$destination\*" -Destination $destination_backup -Recurse -Force
 
-                Write-Logs "Copy-Item -Path `"$_source\*`" -Destination $_destination -Recurse -Force"
-                Copy-Item -Path "$_source\*" -Destination $_destination -Recurse -Force
+                Write-Logs "Copy-Item -Path `"$source\*`" -Destination $destination -Recurse -Force"
+                Copy-Item -Path "$source\*" -Destination $destination -Recurse -Force
             }
         }else{
-            Write-Logs "Copy-Item -Path $_source -Destination $_destination -Recurse -Force"
-            Copy-Item -Path $_source -Destination $_destination -Recurse -Force
+            Write-Logs "Copy-Item -Path $source -Destination $destination -Recurse -Force"
+            Copy-Item -Path $source -Destination $destination -Recurse -Force
         }
     }
 }
