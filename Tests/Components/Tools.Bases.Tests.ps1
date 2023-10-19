c6d18cfd73aafd91475418ffe3ee298daf39dbb8
Zhaopudark <zhaopudark@outlook.com>
2023-10-19 19:03:26 +0800

    Prepare for version v0.0.4

--- Tests/Components/Tools.Bases.Tests.ps1
+++ Tests/Components/Tools.Bases.Tests.ps1
@@ -16,7 +16,7 @@ Describe '[Test Bases Tools]' {
             Format-VersionTo4SegmentFormat -RawVersion 'v0.1' | Should -Be '0.1.0.0'
             Format-VersionTo4SegmentFormat -RawVersion 'v1' | Should -Be '1.0.0.0'
             Format-VersionTo4SegmentFormat -RawVersion 'v1.2.3.4.5.6.7' | Should -Be '1.2.3.4'
-            Format-VersionTo4SegmentFormat -RawVersion 'x1.2.3.4.5.6.7' | Should -Throw "Invalid version number: x1.2.3.4.5.6.7. It should be in the format like vX, vX.X, vX.X.X, vX.X.X.X...."
+            {Format-VersionTo4SegmentFormat -RawVersion 'x1.2.3.4.5.6.7'} | Should -Throw "Invalid version number: x1.2.3.4.5.6.7. It should be in the format like vX, vX.X, vX.X.X, vX.X.X.X...."
         }
     }
 }
