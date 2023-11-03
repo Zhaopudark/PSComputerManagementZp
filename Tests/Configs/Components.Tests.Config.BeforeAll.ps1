272f4ceaceead964592f8ee1c75dc9e1e430fa9d
Zhaopudark <zhaopudark@outlook.com>
2023-11-03 17:46:27 +0800

    Modifiy the organization of all APIs

--- Tests/Configs/Components.Tests.Config.BeforeAll.ps1
+++ Tests/Configs/Components.Tests.Config.BeforeAll.ps1
@@ -1,3 +1,5 @@
 $ErrorActionPreference_backup = $ErrorActionPreference
 $ErrorActionPreference = 'Stop'
+using module ".\Classes\EnvPath.psm1"
+using module ".\Classes\FormattedFileSystemPath.psm1"
 Import-Module "${PSScriptRoot}\..\..\Module\PSComputerManagementZp.psm1" -Force -Scope Local
