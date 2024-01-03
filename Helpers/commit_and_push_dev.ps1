463d4cd6659a2bc4a301804556b85c8c81709fe0
Zhaopudark <zhaopudark@outlook.com>
2024-01-03 17:23:59 +0800

    Add helping scripts for dev

--- Helpers/commit_and_push_dev.ps1
+++ Helpers/commit_and_push_dev.ps1
@@ -9,6 +9,7 @@ if ($currentBranch -eq "dev") {
     # 如果当前分支是 dev 分支，则进行 commit 操作
     git add .  # 添加需要提交的文件（假设你要提交所有文件）
     git commit -m $message
+    git push origin dev:dev
 } else {
     Write-Host "Not on dev branch. Current branch: $currentBranch"
 }
