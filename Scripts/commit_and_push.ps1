9a141b106110222e6512898ffb912460a9f86414
Zhaopudark <zhaopudark@outlook.com>
2024-01-20 13:04:07 +0800

    modify helping scripts

--- Scripts/commit_and_push.ps1
+++ Scripts/commit_and_push.ps1
@@ -5,9 +5,6 @@ param (
 
 $currentBranch = git rev-parse --abbrev-ref HEAD
 if ($currentBranch -eq "main") {
-    git branch -D dev
-    git switch -c dev
-
     # 如果当前分支是 dev 分支，则进行 commit 操作
     . "${PSScriptRoot}/build.ps1"
     git add .  # 添加需要提交的文件（假设你要提交所有文件）
